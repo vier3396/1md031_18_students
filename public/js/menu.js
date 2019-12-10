e1d6956d71db9eeb05fdfb23b9d4e0665aa00dd8
vier3396 <viktor.eriksson.3396@user.uu.se>
2019-12-10 22:20:11 +0100

    add menu

---
+++ public/js/menu.js
@@ -0,0 +1,38 @@
+var food = [
+  {
+  "name":'The Super Burger',
+  "kCal": 1550,
+  "lactose": true,
+  "gluten": false,
+  "img": 'https://cdn.pixabay.com/photo/2017/08/02/22/11/burger-2573682_1280.jpg'
+},
+{
+  'name':'The Veggie delight',
+   'kCal':780,
+    'lactose':true,
+    'gluten':true,
+    'img':'https://live.staticflickr.com/65535/48436455446_163bfdb6ac_b.jpg'
+},
+{
+  'name':'The notABigMac',
+  'kCal':8000,
+  'lactose':true,
+  'gluten':false,
+  'img':'https://live.staticflickr.com/5218/5386641085_42b19cff0d_b.jpg'
+},
+{
+  'name':'The Chicken',
+  'kCal':800,
+  'lactose':true,
+  'gluten':false,
+  'img':'https://cdn.pixabay.com/photo/2017/11/16/05/51/chicken-burger-2953388_1280.jpg'
+},
+{
+  'name':'The Fish',
+  'kCal':100,
+  'lactose':false,
+  'gluten':false,
+  'img':'https://live.staticflickr.com/3072/2572625481_c26bf8ffe2_b.jpg'
+}
+
+]
